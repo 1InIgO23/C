5d1bf7114c381b1c3b2c78381a0e10a8533218b8
1InIgO23 <inigo3211@gmail.com>
2023-07-10 20:17:03 +0200

    C

---
+++ betterCalculator.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int sum(int num1, int num2) {
+    int result = num1 + num2;
+    return result;
+}
+int subtraction(int num1, int num2) {
+    int result = num1 - num2;
+    return result;
+}
+int multiplication(int num1, int num2) {
+    int result = num1 * num2;
+    return result;
+}
+int division(int num1, int num2) {
+    float result = num1 / num2;
+    return result;
+}
+
+int main()
+{
+    int a;
+    int b;
+    char op;
+    
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+
+    while (getchar() != '\n');
+
+    printf("Enter the operator: ");
+    scanf("%c", &op);
+
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+
+    if (op == '+') {
+        int result = sum(a, b);
+        printf("%d", result);
+    } else if (op == '-') {
+        int result = subtraction(a, b);
+        printf("%d", result);
+    } else if (op == '/') {
+        float result = division(a, b);
+        printf("%f", result);
+    } else if (op == '*') {
+        int result = multiplication(a, b);
+        printf("%d", result);
+    } else {
+        printf("Invalid input");
+    }
+    
+    return 0;
+}
